0d93c81985adc89ed923e4226a940e3f0b742f57
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2013-12-11 00:07:24 +0100

    Moduralized translator functions

--- js/music.js
+++ js/music.js
@@ -2,16 +2,25 @@ function playNote(note) {
   MIDI.noteOn(0, note, 100, 0); // 21 - 108
 }
 
+// holds various methods to transform the band into music
 var cumulativeState = 0;
+var transType = "cumulative";
+var caTranslator = {
+  "one_count": function(b) {
+    var state = b.filter(function(value) { return value == 1 }).length;
+    playNote(21 + state);
+  },
+  "cumulative": function(b) {
+    cumulativeState = (cumulativeState + listToInt(band)) % 87;
+    playNote(21 + cumulativeState);
+  }
+};
+
 function dropThePiano() {
   console.log(band.join(""));
   document.getElementById("output").innerHTML = band.join("");
 
-  // How to transform band into music
-  cumulativeState = (cumulativeState + listToInt(band)) % 87;
-  playNote(21 + cumulativeState);
-  /*var state = band.filter(function(value) { return value == 1 }).length;
-  playNote(21 + state);*/
+  caTranslator[transType](band);
 
   nextStep();
 }
